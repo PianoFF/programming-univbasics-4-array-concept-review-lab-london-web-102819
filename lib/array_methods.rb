b332319f5f458cfc15a6fa9c30d65326fc6918cd
Fangfei Chen <fangfei1star@gmail.com>
2019-10-21 17:45:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,10 +15,10 @@ end
 
 
 
-# def find_max_value(array)
-#   array.max
-# end
-# 
-# def find_min_value(array)
-#   array .min
-# end
+def find_max_value(array)
+  array.max
+end
+
+def find_min_value(array)
+  array .min
+end
